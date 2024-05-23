c7b6b2d3b2ac9d429e4c372b3ab1edb19ee49396
Pedro Henrique Mequelim da Silva <pedromequelim@gmail.com>
2024-05-23 12:16:10 -0300

    Creation of Sale repository

---
+++ salesAPI/src/main/java/com/sales/api/salesAPI/repository/SaleRepository.java
@@ -0,0 +1,8 @@
+package com.sales.api.salesAPI.repository;
+
+import com.sales.api.salesAPI.model.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long> {}
